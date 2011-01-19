6670a22d596da270fd47a447812942d59365d403
Klaus Hartl <klaus.hartl@gmail.com>
2011-01-19 10:58:20 +0100

    skeleton with rspec

---
+++ lib/mongoid_omnisocial.rb
@@ -0,0 +1,2 @@
+module MongoidOmnisocial
+end
---
+++ spec/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '3c1ff4026f18b6ecc0a8f61d9ffad0cbff553621e59be9368dde681fc6fe0681f3576040a468d8e53f3d8fd2726e056b2c579329f1ef878be89b31ed85dcf3ef'
